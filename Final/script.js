e1d42b1c16b5d7863413b8e41905fbf9f5bf9c7e
Alex Friedberg <blueal@users.noreply.github.com>
2022-06-01 19:22:11 -0700

    Added Plugin

--- Final/script.js
+++ Final/script.js
@@ -4,7 +4,7 @@ var mymap = L.map('mapid', { center: [47.6062, -122.3321], zoom: 10 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
 
 //Create Layers
-var linkLayer = L.geoJSON(false, { style: "blue" });
+var linkLayer = L.geoJSON(false, { style: "green" });
 var burkeLayer = L.geoJSON(false, { style: "green" });
 var gasworksLayer = L.geoJSON(false, { style: "brown" });
 var southLayer = L.geoJSON(false, { style: "yellow" });
@@ -15,8 +15,20 @@ $.getJSON("./burke.geojson", function (data) { burkeLayer.addData(data); });
 $.getJSON("./gasworks.geojson", function (data) { gasworksLayer.addData(data); });
 $.getJSON("./south.geojson", function (data) { southLayer.addData(data); });
 
-//Add Layers to map
-linkLayer.addTo(mymap);
-burkeLayer.addTo(mymap);
-gasworksLayer.addTo(mymap);
-southLayer.addTo(mymap);
+//Animate Layers into Map
+function animateLayers(speed) {
+    linkLayer.setStyle({snakingSpeed:speed})
+    burkeLayer.setStyle({snakingSpeed:speed})
+    gasworksLayer.setStyle({snakingSpeed:speed})
+    southLayer.setStyle({snakingSpeed:speed})
+
+    linkLayer.addTo(mymap).snakeIn();
+    burkeLayer.addTo(mymap).snakeIn();
+    gasworksLayer.addTo(mymap).snakeIn();
+    southLayer.addTo(mymap).snakeIn();
+}
+
+//Run this when page is fully loaded
+window.onload = (event) => {
+    animateLayers(50);
+};
